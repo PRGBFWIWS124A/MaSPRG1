c01af52ee431d844481d548daa671b48565529d3
MarcSem <Marc.Semrau@edu.fhdw.de>
2024-01-12 12:30:30 +0100

    2

--- Sum.java
+++ Sum.java
@@ -1,5 +1,5 @@
 public class Sum {
     public static void main(String[] args){
-        System.out.println(args[0]+args[1]);
+        System.out.println(Integer.parseInt(args[0])+Integer.parseInt(args[1]));
     }
 }
