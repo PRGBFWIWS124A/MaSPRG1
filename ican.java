cc1d0d242c79d8f3a2cd609d1da688e49d1dd9cb
MarcSem <Marc.Semrau@edu.fhdw.de>
2024-01-26 10:14:10 +0100

    version 1

--- ican.java
+++ ican.java
@@ -1,5 +1,21 @@
 public class ican {
     public static void main(String[] args){
-        System.out.println("Ich kann Programmieren!");
+        System.out.println(product(2,3));
+        System.out.println(squaresum(2,3));
+        warning();
+    }
+    static int product(int x, int y){
+        return x + y;
+    }
+    static int squaresum(int x, int y){
+        return x*x + y*y;
+    }
+    static void output(String content){
+        System.out.println(content);
+        System.out.println("");
+    }
+    static void warning(){
+        System.out.println("WARNUNG");
+        System.out.println();
     }
 }
